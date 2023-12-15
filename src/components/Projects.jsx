8bf3e681b800d65d50e1ca30e152b4d14de9b0f8
coledude04 <cole.alcala04@gmail.com>
2023-12-15 08:58:56 -0500

    Added Sector Sorter Project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import screener from '/src/images/screener.png';
 import bookProject from '/src/images/bookProject.png';
 import PERanker from '/src/images/PERanker.png';
+import sectorSorter from '/src/images/sectorSorter.png';
 
 const ProjCard = (props) => {
     return (
@@ -22,6 +23,7 @@ const Projects = () => {
                 <ProjCard title="Stock Screener" imgSrc={screener} desc="A GUI made in python using TKinter, Yahoo Finance, and pandas. It sorts stocks based on user input and gets data with API's and web-scraping." />
                 <ProjCard title="Book File Holder" imgSrc={bookProject} desc="A GUI made with JavaFX that allows a user to add books with a title, author, and genre. The GUI takes advantage of the JavaFX TableView." />   
                 <ProjCard title="PE Ratio Ranker" imgSrc={PERanker} desc="A Python program that scans all the stocks in the NASDAQ and NYSE and calculates the average and current PE Ratio for each. It then compares them." />   
+                <ProjCard title="Sector Sorter" imgSrc={sectorSorter} desc="A GUI made with python and TKinter. This GUI allows the user to select sectors to view stocks from and values to display for each stock." />
             </div>
         </div>
     );
